af5d06e39ed2c137c988a5c2a68ecb98ccf4d4ac
patricksawtell <patrick.sawtell@gmail.com>
2016-12-15 20:44:54 -0800

    logic

--- flex.js
+++ flex.js
@@ -1,9 +1,17 @@
-const panels = document.querySelectorAll('.panels');
+const panels = document.querySelectorAll('.panel');
 
 function toggleOpen() {
 	console.log('hello');
 	this.classList.toggle('open');
 }
 
-panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+function toggleActive(e) {
+	console.log(e.propertyName);
+	if (e.propertyName.includes('flex')) {
+		this.classList.toggle('open-active');
+	}
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen)); //listen for a click on each panel and run toggleOpen
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive)); //when finished toggle openActive
 
