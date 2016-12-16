ec28f0b2fa1ed5703537d0ee4e5c713f756a30cd
patricksawtell <patrick.sawtell@gmail.com>
2016-12-15 20:49:32 -0800

    clean JavaScript

--- flex.js
+++ flex.js
@@ -1,12 +1,12 @@
 const panels = document.querySelectorAll('.panel');
 
 function toggleOpen() {
-	console.log('hello');
+	// console.log('hello');
 	this.classList.toggle('open');
 }
 
 function toggleActive(e) {
-	console.log(e.propertyName);
+	// console.log(e.propertyName);
 	if (e.propertyName.includes('flex')) {
 		this.classList.toggle('open-active');
 	}
