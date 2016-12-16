beec9fafd7dda04f5a2678df004475ee96bb7600
patricksawtell <patrick.sawtell@gmail.com>
2016-12-15 20:33:50 -0800

    JS logic

---
+++ flex.js
@@ -0,0 +1,9 @@
+const panels = document.querySelectorAll('.panels');
+
+function toggleOpen() {
+	console.log('hello');
+	this.classList.toggle('open');
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+
